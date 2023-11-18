a24048a646f1ce18d38e947f6f320f1b234961c6
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-11-17 22:15:17 -0600

    actualice librerias

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const path = require('path');
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults()
+const cors = require('cors')
 
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
 const port = process.env.PORT || 8080;
 
+const corsOption ={
+    origin:'*',
+    methods:['GET','PUT','POST','DELETE','OPTIONS'],
+    allowedHeaders:['Content-type','Authorization']
+};
+
 const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
 
 // /!\ Bind the router db to the app
 app.db = router.db
 
-// You must apply the auth middleware before the router
+// You must apply the auth middleware before the router´
+app.use(cors(corsOption))
 app.use(middlewares);
 app.use(rules);
 app.use(auth);
